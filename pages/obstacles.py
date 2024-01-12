8b521958f6ad1f29e4fbb87bec1d87d63477f75c
unknown <Tealc95>
2024-01-12 20:59:58 +0100

    Beautifull interface

--- pages/obstacles.py
+++ pages/obstacles.py
@@ -2,11 +2,52 @@ import streamlit as st
 import crobstacles
 import os
 from io import StringIO
+import pandas as pd
 
 
 st.set_page_config(layout="wide")
 
 st.header("CONTROLE DES FICHIERS OBSTACLES")
+
+def returnTagValue(p_tag_name,p_liste_tags):
+    if p_tag_name in p_liste_tags:
+        if p_liste_tags[p_tag_name] == '':
+            return '*vide*'
+        return p_liste_tags[p_tag_name]
+    else:
+        return "absent"
+
+def voirEnteteObstacle(p_liste_tags,p_error):
+    st.divider()
+    _color = 'green'
+    if p_error is True:
+        _color = 'red'
+
+    _text =f'#### NOM OBSTACLE : :{_color}[{returnTagValue("NOM",p_liste_tags)}]'
+    st.markdown(_text)
+    _text = f"ID METIER : :blue[{returnTagValue("IDMETIER",p_liste_tags)}]"
+    _text += f" | ANCIENNETE : :blue[{returnTagValue("ANCIENNETE",p_liste_tags)}]"
+    _text += f" | NATURE OBSTACLE : :blue[{returnTagValue("NATUREOBST",p_liste_tags)}]"
+    _text += f" | NOMBRE DE DEBOUCHES : :blue[{returnTagValue("NBRDEBOUCHE",p_liste_tags)}]"
+    _text += f" | NOMBRE DE SOMMETS : :blue[{returnTagValue("NBRSOMMET",p_liste_tags)}]"
+
+    st.markdown(_text)
+
+    _text = f"DE LA LIGNE : :blue[{returnTagValue("DE LA LIGNE",p_liste_tags)}]"
+    _text += f" | PKDEBUT : :blue[{returnTagValue("PKDEBUT",p_liste_tags)}]"
+    _text += f" | PKFIN : :blue[{returnTagValue("PKFIN",p_liste_tags)}]"
+    st.markdown(_text)
+
+    _text = f"DE LA VOIE : :blue[{returnTagValue("DE LA VOIE",p_liste_tags)}]"
+    _text += f" | LIBELLE VOIE : :blue[{returnTagValue("LIBELLE VOIE",p_liste_tags)}]"
+    _text += f" | NOM LOCAL VOIE : :blue[{returnTagValue("NOM LOCAL VOIE",p_liste_tags)}]"
+    _text += f" | VOIE DE SERVICE : :blue[{returnTagValue("VOIE DE SERVICE",p_liste_tags)}]"
+
+    st.markdown(_text)
+
+
+    st.divider()
+
 
 def isFileLF(p_lines):
     _return = True
@@ -21,9 +62,11 @@ def isFileLF(p_lines):
 def isOuvrable():
     return True
 
-uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = st.file_uploader("Choisissez un fichier obstacle à analyser")
 if uploaded_file is not None:
 #    st.write(uploaded_file)
+
+    _error = False
 
     _name = uploaded_file.name
 #    st.write(uploaded_file.readlines())
@@ -40,6 +83,7 @@ if uploaded_file is not None:
     string_datas = stringio.readlines()
 
     if not isFileLF(string_datas):
+        _error = True
         st.error("Ce n'est pas un format LF.")
 
     _liste_erreur = []
@@ -47,14 +91,25 @@ if uploaded_file is not None:
     _liste_sommet = []
     _liste_entite = []
 
-    crobstacles.controleObstacle("", "", string_datas, _liste_erreur, _liste_sommet, _liste_entite, _dico_tags)
+    _return = crobstacles.controleObstacle("", "", string_datas, _liste_erreur, _liste_sommet, _liste_entite, _dico_tags)
 
-    if len(_liste_erreur)>0:
+    if _return == 99:
+        _error = True
+        st.error("Ce n'est pas un fichier OBSTACLE.")
+        st.stop()
+
+    if len(_liste_erreur) > 0:
+        _error = True
+
+    if _dico_tags is not None:
+        voirEnteteObstacle(_dico_tags,_error)
+
+
+    if len(_liste_erreur) > 0:
         st.subheader("Liste des anomalies")
         st.write(_liste_erreur)
     st.subheader("Liste des champs dans le fichier")
     st.write(string_datas)
-
     st.stop()
 
 pass
